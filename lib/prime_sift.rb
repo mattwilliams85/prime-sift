a7c3852f4d7f2f1af5aeb49cd4b9a77877b268cf
Matt Williams & moof mayeda <student@epicodus.com>
2014-07-23 10:02:00 -0700

    refactored

--- lib/prime_sift.rb
+++ lib/prime_sift.rb
@@ -1,9 +1,7 @@
 def prime_sift(number)
-  prime = 2
   numbers = (2..number).to_a
   numbers.each do |current_number|
-    numbers.delete_if {|integer| integer%prime == 0 && integer != prime}
-    prime +=1
+    numbers.delete_if {|integer| integer%current_number == 0 && integer != current_number}
   end
 end
 
